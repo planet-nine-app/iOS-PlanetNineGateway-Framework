66a4595f3adfc36509c2bb6f52f592b25c7aa23e
Zach Babb <zach@planetnine.app>
2019-01-29 16:35:20 -0800

    Initial commit of gateway framework

---
+++ PlanetNineGateway/One Time Gateway/OneTimeGateway.swift
@@ -0,0 +1,20 @@
+//
+//  OneTimeGateway.swift
+//  PlanetNineGateway
+//
+//  Created by Zach Babb on 1/29/19.
+//  Copyright © 2019 Planet Nine. All rights reserved.
+//
+
+import Foundation
+
+public class OneTimeGateway {
+    
+    public init() {
+        
+    }
+    
+    public func test() {
+        print("Here I am")
+    }
+}
